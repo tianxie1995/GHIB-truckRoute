d74d6d2b5d24fdc8d48f67f62969bce54a2583ee
unknown <tianxie@umich.edu>
2019-02-18 16:55:51 -0500

    commit

--- NetworkBuilder.py
+++ NetworkBuilder.py
@@ -2,40 +2,48 @@
 
 import arcpy
 from arcpy import env
-import config 
+import config
 import os
+
 
 def select_network_by_mask(road, mask, plaza_mask, output):
     """
     Clip the road network by a specified mask
 
     Input:
-    :road - string - file name of the road layer
-    :mask - string - file name of the mask layer
-    :output - string - file name of the output network
+    :road   - str -  file name of the road layer
+    :mask   - str -  file name of the mask layer
+    :output - str -  file name of the output network
 
     Output:
-    :output - string - file name of the output network
+    :output - str -  file name of the output network
     """
     if not arcpy.Exists(output):
-        arcpy.MakeFeatureLayer_management(road, 'road_lyr')
-        print("match:{}".format(int(arcpy.GetCount_management('road_lyr')[0])))
-        arcpy.SelectLayerByLocation_management('road_lyr', "INTERSECT", mask)
-        print("match:{}".format(int(arcpy.GetCount_management('road_lyr')[0])))
-        arcpy.SelectLayerByLocation_management('road_lyr', "INTERSECT", plaza_mask,
-                                                 "", "REMOVE_FROM_SELECTION")
-        print("match:{}".format(int(arcpy.GetCount_management('road_lyr')[0])))
-        arcpy.SelectLayerByAttribute_management('road_lyr', 
-                                            'SUBSET_SELECTION', 
-                                            '"FCC" <> \'A11\'')
-        print("match:{}".format(int(arcpy.GetCount_management('road_lyr')[0])))
-        arcpy.CopyFeatures_management('road_lyr', output)
-        return output 
+        arcpy.MakeFeatureLayer_management(road, "road_lyr")
+        print("match:{}".format(int(arcpy.GetCount_management("road_lyr")[0])))
+        arcpy.SelectLayerByLocation_management("road_lyr", "INTERSECT", mask)
+        print("match:{}".format(int(arcpy.GetCount_management("road_lyr")[0])))
+        arcpy.SelectLayerByLocation_management(
+            "road_lyr", "INTERSECT", plaza_mask, "", "REMOVE_FROM_SELECTION"
+        )
+        print("match:{}".format(int(arcpy.GetCount_management("road_lyr")[0])))
+        arcpy.SelectLayerByAttribute_management(
+            "road_lyr", "SUBSET_SELECTION", "\"FCC\" <> 'A11'"
+        )
+        print("match:{}".format(int(arcpy.GetCount_management("road_lyr")[0])))
+        arcpy.CopyFeatures_management("road_lyr", output)
+        return output
     else:
         print(output, " already exist")
-        return output 
+        return output
 
-def calculate_time_by_speed(road):
+
+def add_time_roadLevel(road):
+    """
+    Calculate the time cost and road level
+    Input: 
+    :road - str - the path of the road file 
+    """
     arcpy.AddField_management(road, "TIME_COST", "DOUBLE")
     arcpy.AddField_management(road, "LEVEL", "SHORT")
     arcpy.AddField_management(road, "OldTrkRt", "SHORT")
@@ -48,10 +56,10 @@ def getTime(fcc, length):
     elif digit < 30:
         return ((float(length)/ 5280 )/ 40 )*3600
     elif digit < 40:
-        return ((float(length)/ 5280 )/ 30 )*3600
+        return ((float(length)/ 5280 )/ 25 )*3600
     else:
         return ((float(length)/ 5280 )/ 20 )*3600"""
-    codeblock2 = """
+    codeblock2 = """Y
 def getLevel(fcc):
     digit = int((fcc.strip())[1:])
     if digit < 20:
@@ -61,7 +69,22 @@ def getLevel(fcc):
     else:
         return 3"""
 
-    codeblock3 = """
+    expression1 = "getTime(!FCC!, float(!LENGTH!))"
+    arcpy.CalculateField_management(road, "TIME_COST", expression1, "PYTHON", codeblock)
+    expression2 = "getLevel(!FCC!)"
+    arcpy.CalculateField_management(road, "LEVEL", expression2, "PYTHON", codeblock2)
+
+
+def add_truck_route(road):
+    """
+    add truck route attribute to the road file 
+    Input: 
+    :road - str - the path of the road file 
+    """
+
+    arcpy.AddField_management(road, "OldTrkRt", "SHORT")
+
+    codeblock = """
 def getOldTrkRt(FENAME):
     if FENAME in [ 'Wyoming' ,
         'Wyoming/E I 94' , 'Wyoming/US 12' ,
@@ -78,21 +101,28 @@ def getOldTrkRt(FENAME):
     else:
         return 0"""
 
-    expression1 = "getTime(!FCC!, float(!LENGTH!))"
-    out_table = arcpy.CalculateField_management(road, "TIME_COST", expression1, "PYTHON", 
-                                    codeblock)
-    expression2 = "getLevel(!FCC!)"
-    out_table = arcpy.CalculateField_management(road, "LEVEL", expression2, "PYTHON", 
-                                    codeblock2)
+    expression = "getOldTrkRt(!FENAME!)"
+    arcpy.CalculateField_management(road, "OldTrkRt", expression, "PYTHON", codeblock)
 
-    expression3 = "getOldTrkRt(!FENAME!)"
-    out_table = arcpy.CalculateField_management(road, "OldTrkRt", expression3, "PYTHON", 
-                                    codeblock3)
-    return out_table
 
 def preprocess_network(road, mask, plaza_mask, output):
+    """
+    Preprocess the network for network building 
+    Input: 
+    :road - str -       path to the whole road file
+    :mask - str -       path to mask of research area 
+    :plaza_mast - str - path to the plaza mask 
+    :output - str -     path to put the output road file 
+    Output:
+    None 
+    """
+    # arcpy settings
     env.workspace = config.working_folder
     env.overwriteOutput = True
     env.parallelProcessingFactor = "100%"
+    # select road by mask
     selected_roads = select_network_by_mask(road, mask, plaza_mask, output)
-    out_table = calculate_time_by_speed(selected_roads)
+    # add fields to the road
+    add_time_roadLevel(selected_roads)
+    add_truck_route(selected_roads)
+
